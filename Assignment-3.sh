ed9bfef79361282757019fdf4c2c1de63ca1558a
Vaishnavi1898 <68180753+Vaishnavi1898@users.noreply.github.com>
2021-04-29 19:14:34 +0530

    Delete Assignment-3.sh

--- Assignment-3.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-Firstname=Vaishnavi
-Lastname=Nimbalkar
-
-#this command sets value Vaishnavi and Nimbalkar to Firstname and Lastname variables
-
-echo "First name is $Firstname"
-echo "Lastname is $Lastname"
